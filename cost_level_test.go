e46a7eadeb8275dbb1b05151952c05139e7ef246
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-07-15 20:41:18 +0300

    add enum test

---
+++ cost_level_test.go
@@ -0,0 +1,15 @@
+package meander_test
+
+import (
+	"testing"
+	"github.com/cheekybits/is"
+	"github.com/ignatev/meander"
+)
+func TestCostValues(t *testing.T) {
+	is := is.New(t)
+	is.Equal(int(meander.Cost1), 1)
+	is.Equal(int(meander.Cost2), 2)
+	is.Equal(int(meander.Cost3), 3)
+	is.Equal(int(meander.Cost4), 4)
+	is.Equal(int(meander.Cost5), 5)
+}
