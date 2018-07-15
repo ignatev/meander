82701d28b8e18d256b93eec1927cefd922d8933b
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-07-15 21:11:38 +0300

    add test for costRange

--- cost_level.go
+++ cost_level.go
@@ -1,5 +1,10 @@
 package meander
 
+import (
+	"strings"
+	"errors"
+)
+
 type Cost int8
 const (
 	_ Cost = iota
@@ -25,4 +30,28 @@ func(l Cost) String() string {
 		}
 	}
 	return "invalid"
+}
+
+func ParseCost(s string) Cost {
+	return costStrings[s]
+}
+
+type CostRange struct {
+	From Cost
+	To Cost
+}
+
+func (r CostRange) String() string {
+	return r.From.String() + "..." + r.To.String()
+}
+
+func ParseCostRange(s string) (CostRange, error) {
+	var r CostRange
+	segs := strings.Split(s, "...")
+	if len(segs) != 2 {
+		return r, errors.New("invalid cost range")
+	}
+	r.From = ParseCost(segs[0])
+	r.To = ParseCost(segs[1])
+	return r, nil
 }
